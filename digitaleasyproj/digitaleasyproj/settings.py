eb09017fe6da1161c9d857b5b8b3bfb7e3ccfebd
FelipeAFV <felipeafigueroav@gmail.com>
2022-06-26 12:46:51 -0400

    changes on allowed cors origins

--- digitaleasyproj/digitaleasyproj/settings.py
+++ digitaleasyproj/digitaleasyproj/settings.py
@@ -146,8 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", 'http://127.0.0.0.1', 'https://127.0.0.0.1','*']
+#CORS_ALLOWED_ORIGINS = ["http://localhost:3000", 'http://127.0.0.0.1', 'https://127.0.0.0.1']
 
+CORS_ALLOWED_ORIGIN_REGEXES = ['*']
 
 ##CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
