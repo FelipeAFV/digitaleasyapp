a587acedec1b24de63265c86de82f93f3a25fd79
FelipeAFV <felipeafigueroav@gmail.com>
2022-06-26 12:26:47 -0400

    comments and change to allow all cors hosts

--- digitaleasyproj/digitaleasyproj/settings.py
+++ digitaleasyproj/digitaleasyproj/settings.py
@@ -148,6 +148,8 @@ LOGIN_URL = '/login'
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", 'http://127.0.0.0.1', 'https://127.0.0.0.1']
 
+CORS_ALLOW_ALL_ORIGINS: True
+
 ##CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
     "DELETE",
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# stop script execution when error
 set -e
 
+#apply migrations
 python manage.py migrate
 
+# move all static files to STATIC_ROOT folder
 python manage.py collectstatic --noinput
 
+# Start wsgi server on port 9000
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module  digitaleasyproj.wsgi
